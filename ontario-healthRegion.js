0a800d9b6b284720c366f523308fa279dfa0ff89
dhillonks <52294825+dhillonks@users.noreply.github.com>
2020-06-30 13:45:42 -0400

    Removed redundant files

--- ontario-healthRegion.js
+++
@@ -1,5 +0,0 @@
-const URL = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350';
-
-fetch(URL)
-    .then((res) => d3.csv(res))
-    .then((data) => console.log(data));
